67796f76e06d0622ec844448ee50d374b95830f0
Laís Galvão <laisgbueno62@gmail.com>
2022-10-06 20:18:46 -0300

    api change

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export const api = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api',
-  baseURL: 'https://api.gold.shinier.com.br/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
